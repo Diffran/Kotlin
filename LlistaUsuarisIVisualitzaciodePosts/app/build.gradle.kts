b9d77872acd582860cdf7ad15cd81fbc2c6133aa
Diffran <148889182+Diffran@users.noreply.github.com>
2024-10-19 14:47:36 +0200

    tocant el gradle

--- LlistaUsuarisIVisualitzaciodePosts/app/build.gradle.kts
+++ LlistaUsuarisIVisualitzaciodePosts/app/build.gradle.kts
@@ -69,7 +69,8 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
